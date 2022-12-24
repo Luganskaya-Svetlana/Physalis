3651781658f003089916c73849be83f03bf35def
Luganskaya Sveta <lugansckajas@yandex.ru>
2022-12-25 00:32:17 +0300

    improve admin page

--- physalisproject/problems/admin.py
+++ physalisproject/problems/admin.py
@@ -7,7 +7,18 @@ from .models import Category, PartOfEGE, Problem, Source, Tag, TypeInEGE
 class ProblemAdmin(admin.ModelAdmin):
     fields = ('text', 'solution', 'answer', 'complexity', 'source', 'category',
               'tags', 'part_ege', 'type_ege')
+    list_display = ('id', 'author', 'date')
     filter_horizontal = ('tags',)
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.author = request.user
+        super().save_model(
+            request=request,
+            obj=obj,
+            form=form,
+            change=change
+        )
 
 
 admin.site.register(Category)
