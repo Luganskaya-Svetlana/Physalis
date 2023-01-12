6c6871f0a6035a935276847edab3bc4f808bac2f
Luganskaya Sveta <lugansckajas@yandex.ru>
2023-01-12 17:59:23 +0300

    rewrite problems verbose name in lowercase

--- physalisproject/problems/apps.py
+++ physalisproject/problems/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class ProblemsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'problems'
-    verbose_name = 'Задачи'
+    verbose_name = 'задачи'
