b98eac0d1cc858a9dc05586671e8141ee7b2c80e
Luganskaya Sveta <lugansckajas@yandex.ru>
2023-02-04 22:43:23 +0300

    add pages with problems of each type

--- physalisproject/typesinege/urls.py
+++ physalisproject/typesinege/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 app_name = 'typesinege'
 urlpatterns = [
-    path('', views.TypesView.as_view(), name='list')
+    path('', views.TypesView.as_view(), name='list'),
+    re_path(r'^(?P<number>[1-9]\d*)/$', views.ProblemsView.as_view(),
+            name='problems')
 ]
--- physalisproject/typesinege/views.py
+++ physalisproject/typesinege/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic.list import ListView
-from problems.models import TypeInEGE
+from problems.models import Problem, TypeInEGE
 
 
 class TypesView(ListView):
@@ -9,3 +10,14 @@ class TypesView(ListView):
 
     def get_queryset(self):
         return super().get_queryset().order_by('number')
+
+
+class ProblemsView(ListView):
+    model = Problem
+    template_name = 'typesinege/problems.html'
+    context_object_name = 'problems'
+
+    def get_queryset(self):
+        number = self.kwargs['number']
+        self.type = get_object_or_404(TypeInEGE, number=number)
+        return Problem.objects.filter(type_ege=self.type)
