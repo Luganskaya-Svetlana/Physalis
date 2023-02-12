2cb3e4002bb868e4d0b33f23ec06d3830e7cf708
Luganskaya Sveta <lugansckajas@yandex.ru>
2023-02-12 15:44:29 +0300

    add page with all problems

--- physalisproject/problems/urls.py
+++ physalisproject/problems/urls.py
@@ -1,9 +1,10 @@
-from django.urls import re_path
+from django.urls import path, re_path
 
 from . import views
 
 app_name = 'problems'
 urlpatterns = [
+    path('', views.ProblemsView.as_view(), name='list'),
     re_path(r'^(?P<pk>[1-9]\d*)/$', views.ProblemView.as_view(),
             name='detail'),
 ]
