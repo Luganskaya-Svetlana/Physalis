1c25b7650c0fc927e8c54dd70053d24b0fc76fa3
pozitron57 <lisakov57@gmail.com>
2023-02-22 23:33:44 +0100

    upd db 112-124; 30

--- physalisproject/problems/templatetags/markdown_extras.py
+++ physalisproject/problems/templatetags/markdown_extras.py
@@ -4,26 +4,10 @@ from django.template.defaultfilters import stringfilter
 import markdown as md
 
 
-def replace_by_dic(el, dic):
-    for i, j in dic.items():
-        el = el.replace(i, j)
-    return el
-
-
-abbr = {
-    'table_ab': 'Ответ:<table class="change"><tr><td>А</td><td>Б</td></tr>\
-                 <tr><td>&nbsp;</td><td></td></tr></table>',
-    'table_abv': 'Ответ:<table class="change"><tr><td>А</td><td>Б</td>\
-                  <td>В</td></tr><tr><td>&nbsp;</td><td></td><td></td></tr>\
-                  </table>'
-}
-
-
 register = template.Library()
 
 
 @register.filter()
 @stringfilter
 def markdown(value):
-    return md.markdown(replace_by_dic(value, abbr),
-                       extensions=['markdown.extensions.fenced_code'])
+    return md.markdown(value, extensions=['markdown.extensions.fenced_code'])
