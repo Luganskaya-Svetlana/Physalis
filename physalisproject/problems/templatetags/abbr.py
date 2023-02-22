22682c97d131cddb76bb19904c16a329ef2afe97
pozitron57 <lisakov57@gmail.com>
2023-02-22 23:32:50 +0100

    add abbr filter

---
+++ physalisproject/problems/templatetags/abbr.py
@@ -0,0 +1,26 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+
+
+def replace_by_dic(el, dic):
+    for i, j in dic.items():
+        el = el.replace(i, j)
+    return el
+
+
+abbr_dic = {
+    'table_ab': 'Ответ:<table class="change"><tr><td>А</td><td>Б</td></tr>\
+                 <tr><td>&nbsp;</td><td></td></tr></table>',
+    'table_abv': 'Ответ:<table class="change"><tr><td>А</td><td>Б</td>\
+                  <td>В</td></tr><tr><td>&nbsp;</td><td></td><td></td></tr>\
+                  </table>'
+}
+
+
+register = template.Library()
+
+
+@register.filter()
+@stringfilter
+def abbr(value):
+    return replace_by_dic(value, abbr_dic)
