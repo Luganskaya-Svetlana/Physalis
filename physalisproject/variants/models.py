23ee3272d52d0295e1f5838886591d2c75ff52fc
Luganskaya Sveta <lugansckajas@yandex.ru>
2023-03-20 21:15:09 +0300

    add variants

---
+++ physalisproject/variants/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from problems.models import Problem
+
+
+class Variant(models.Model):
+    COMLEXITIES = ('Попроще', 'Нормально', 'Посложнее', 'Дичь')
+    CHOICES = [(elem, elem) for elem in COMLEXITIES]
+    problems = models.ManyToManyField(Problem,
+                                      verbose_name='задачи')
+    text = models.TextField('комментарии', blank=True, null=True)
+    complexity = models.CharField('сложность', choices=CHOICES, max_length=100)
+    is_full = models.BooleanField('полный вариант', default=True)
+    show_answers = models.BooleanField('показать решения', default=True)
+
+    class Meta:
+        verbose_name = 'вариант'
+        verbose_name_plural = 'варианты'
+        default_related_name = 'variants'
+
+    def get_problems(self):
+        return [
+            pv.problem for pv in self.problems.through.objects
+                                     .filter(variant_id=self.id)
+                                     .order_by('problem__type_ege__number')
+        ]
+
+    def __str__(self):
+        return f'вариант с id {self.id}'
