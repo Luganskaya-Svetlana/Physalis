513cc64ff3b6ef7a0c686cc7a21e56a50fc435dd
Luganskaya Sveta <lugansckajas@yandex.ru>
2023-03-24 19:28:24 +0300

    add variants filtering

---
+++ physalisproject/variants/filters.py
@@ -0,0 +1,8 @@
+from django_filters import FilterSet
+from variants.models import Variant
+
+
+class VariantFilter(FilterSet):
+    class Meta:
+        model = Variant
+        fields = ['is_full', 'complexity']
--- physalisproject/variants/views.py
+++ physalisproject/variants/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.list import ListView
+from variants.filters import VariantFilter
 from variants.models import Variant
 
 
@@ -6,6 +7,16 @@ class VariantsView(ListView):
     model = Variant
     template_name = 'variants/variants_list.html'
     context_object_name = 'variants'
+    filter = VariantFilter
 
     def get_queryset(self):
-        return Variant.objects.list()
+        queryset = Variant.objects.list()
+        self.filterset = self.filter(self.request.GET, queryset=queryset)
+        return self.filterset.qs.order_by('id')
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['filter'] = VariantFilter(self.request.GET,
+                                       queryset=self.get_queryset())
+        data['title'] = 'Варианты'
+        return data
