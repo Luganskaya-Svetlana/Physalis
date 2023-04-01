bbf60dc3bcdc48b18197a289137e0b65a8b3ffcd
Luganskaya Sveta <lugansckajas@yandex.ru>
2023-04-01 21:08:23 +0300

    add field answer_slug

--- physalisproject/variants/models.py
+++ physalisproject/variants/models.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from problems.models import Problem
 from variants.managers import VariantManager
 
+from .validators import validate_answer_slug
+
 
 class Variant(models.Model):
     objects = VariantManager()
@@ -14,6 +16,10 @@ class Variant(models.Model):
     is_full = models.BooleanField('полный вариант', default=True)
     show_answers = models.BooleanField('показать решения', default=True)
     date = models.DateField('дата добавления', auto_now_add=True)
+    answer_slug = models.SlugField('slug для получения ответов',
+                                   max_length=4, unique=False,
+                                   blank=True, default='foxy',
+                                   validators=[validate_answer_slug])
 
     class Meta:
         verbose_name = 'вариант'
@@ -25,8 +31,17 @@ class Variant(models.Model):
                     .order_by('type_ege__number')
                     .only('text', 'type_ege__number'))
 
+    def get_answers(self):
+        return (self.problems
+                    .order_by('type_ege__number')
+                    .only('solution', 'answer', 'type_ege__max_score'))
+
     def __str__(self):
         return f'вариант с id {self.id}'
 
     def get_absolute_url(self):
         return reverse('variants:detail', kwargs={'pk': self.pk})
+
+    def get_answers_url(self):
+        return reverse('variants:answers', kwargs={'pk': self.pk,
+                                                   'slug': self.answer_slug})
