2de7aaffb6e0f3607b550d6b1f21efbd1a197bf9
Luganskaya Sveta <lugansckajas@yandex.ru>
2023-04-02 13:02:28 +0300

    correct filter name

--- physalisproject/problems/filters.py
+++ physalisproject/problems/filters.py
@@ -1,16 +1,21 @@
 import django_filters
 
-from .models import Problem
+from .models import Problem, PartOfEGE
 
 
 class ProblemFilter(django_filters.FilterSet):
     complexity = django_filters.RangeFilter(field_name='complexity')
     text = django_filters.CharFilter(lookup_expr='icontains')
+    part_ege = django_filters.filters.ModelChoiceFilter(
+        field_name='type_ege__part_ege',
+        queryset=PartOfEGE.objects.all(),
+        label='Часть ЕГЭ'
+    )
 
     class Meta:
         model = Problem
         fields = ['id', 'text', 'author', 'category', 'subcategory', 'source',
-                  'type_ege__part_ege']
+                  'part_ege']
 
 
 class ProblemTypeFilter(django_filters.FilterSet):
