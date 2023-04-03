ec91f9068f098cc39e1866b400a1686160bb1141
pozitron57 <lisakov57@gmail.com>
2023-04-03 22:34:56 +0000

    ziamath, nav

--- physalisproject/problems/templatetags/ziamath.py
+++ physalisproject/problems/templatetags/ziamath.py
@@ -10,16 +10,12 @@ register = template.Library()
 
 @register.filter()
 @stringfilter
-
 def ziamath(string):
     groups = re.split(r'(\${1,2}[^${]+\$*)', string, flags=re.DOTALL)
     for i in range(len(groups)):
         if groups[i].startswith('$') and groups[i].endswith('$'):
-            # Убираем доллары и применяем функцию из ziamath
             groups[i] = zm.Math.fromlatex(groups[i].strip('$')).svg()
     return ''.join(groups)
-
-
 
 #def ziamath(value):
     #return zm.zmath.Math.fromlatextext(value).svg()
@@ -31,12 +27,10 @@ def ziamath(string):
 #def ziamath(value):
     #return re.sub('\$(.*?)\$', zi, value, flags=re.M)
 
-
-## Работает с одиночными долларами
+# Работает с одиночными долларами
 #def ziamath(string):
     #groups = string.split('$')
     #for i in range(1, len(groups), 2):
         #groups[i] = zm.Math.fromlatex(groups[i]).svg()
     #result = ''.join(groups)
     #return result
-
