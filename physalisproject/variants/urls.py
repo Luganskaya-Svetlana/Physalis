b66f5cbf6f3a30e2f71cc89219ac5b5e3194d7d7
pozitron57 <lisakov57@gmail.com>
2023-04-14 13:20:18 +0000

    add caching for variant_detail and variant_answer

--- physalisproject/variants/urls.py
+++ physalisproject/variants/urls.py
@@ -5,9 +5,6 @@ from . import views
 app_name = 'variants'
 urlpatterns = [
     path('', views.VariantsView.as_view(), name='list'),
-    re_path(r'^(?P<pk>[1-9]\d*)-(?P<slug>[a-z]{4})/$',
-            views.VariantAnswerView.as_view(),
-            name='answers'),
-    re_path(r'^(?P<pk>[1-9]\d*)/$', views.VariantView.as_view(),
-            name='detail'),
+    re_path(r'^(?P<pk>[1-9]\d*)-(?P<slug>[a-z]{4})/$', views.VariantAnswerView.as_view(), name='answers'),
+    re_path(r'^(?P<pk>[1-9]\d*)/$', views.VariantView.as_view(), name='detail'),
 ]
