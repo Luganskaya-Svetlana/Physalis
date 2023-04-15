1ca4bee1de17b742d9fa0d6c86485c4426c0597f
Luganskaya Sveta <lugansckajas@yandex.ru>
2023-04-15 18:09:16 +0300

    pep8 again

--- physalisproject/problems/urls.py
+++ physalisproject/problems/urls.py
@@ -7,5 +7,6 @@ app_name = 'problems'
 
 urlpatterns = [
     path('', views.problems_view, name='list'),
-    re_path(r'^(?P<pk>[1-9]\d*)/$', views.ProblemView.as_view(), name='detail'),
+    re_path(r'^(?P<pk>[1-9]\d*)/$', views.ProblemView.as_view(),
+            name='detail'),
 ]
