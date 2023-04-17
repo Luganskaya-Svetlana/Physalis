d00f6c57ab1a2c98b8d45752d6e38956f0e00f02
Luganskaya Sveta <lugansckajas@yandex.ru>
2023-04-17 19:05:02 +0300

    change label of text filter

--- physalisproject/problems/filters.py
+++ physalisproject/problems/filters.py
@@ -5,7 +5,8 @@ from .models import Problem, PartOfEGE
 
 class ProblemFilter(django_filters.FilterSet):
     complexity = django_filters.RangeFilter(field_name='complexity')
-    text = django_filters.CharFilter(lookup_expr='icontains')
+    text = django_filters.CharFilter(lookup_expr='icontains',
+                                     label='Поиск по тексту')
     part_ege = django_filters.filters.ModelChoiceFilter(
         field_name='type_ege__part_ege',
         queryset=PartOfEGE.objects.all(),
