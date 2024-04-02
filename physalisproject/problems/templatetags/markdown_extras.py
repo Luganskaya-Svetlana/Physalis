5811132fc1b9df2bc46edeeea566eff24b0fd114
pozitron57 <lisakov57@gmail.com>
2024-04-02 18:31:41 +0300

    markdown toc extension set toc_depth to 2-6

--- physalisproject/problems/templatetags/markdown_extras.py
+++ physalisproject/problems/templatetags/markdown_extras.py
@@ -1,6 +1,5 @@
 from django import template
 from django.template.defaultfilters import stringfilter
-
 import markdown as md
 
 
@@ -10,7 +9,15 @@ register = template.Library()
 @register.filter()
 @stringfilter
 def markdown(value):
-    return md.markdown(value, extensions=['markdown.extensions.fenced_code',
-                                          'tables',
-                                          'toc',
-                                          'footnotes'])
+    return md.markdown(value, extensions=[
+        'markdown.extensions.fenced_code',
+        'markdown.extensions.tables',
+        'markdown.extensions.toc',
+        'markdown.extensions.footnotes'
+    ],
+    extension_configs={
+        'markdown.extensions.toc': {
+            'toc_depth': '2-6',
+        },
+    })
+
