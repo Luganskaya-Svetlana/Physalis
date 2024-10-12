c419412d914bf2a4e93b6cf7bc8a477f84556576
pozitron57 <lisakov57@gmail.com>
2024-10-12 17:51:20 +0300

    print ziamath version

--- physalisproject/problems/views.py
+++ physalisproject/problems/views.py
@@ -15,6 +15,12 @@ class ProblemView(DetailView):
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
+
+        # Для проверки версии ziamath: добавить в templates/problems/problem_detail.html:
+        # Используемая версия ziamath: {{ ziamath_version }}
+        # import ziamath
+        # data['ziamath_version'] = ziamath.__version__
+
         problem_pk = self.kwargs['pk']
         # для временной навигации:
         data['next_problem'] = (Problem.objects
